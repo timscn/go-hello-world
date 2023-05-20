016a4dbf405e9092cbf32892834e350817f7e87f
timscn <timscn@gmail.com>
2023-05-20 16:50:02 -0400

    pointer updates

--- leetcfirst.go
+++ leetcfirst.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func main6666() {
 	// 1480. Running Sum of 1D Array
 	ids := []int{3, 1, 2, 10, 1}
 	for i := 1; i < len(ids); i++ {
--- pointers.go
+++ pointers.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func getAddressOfVar(digit int) *int {
 	return &digit
+}
+
+func main() {
+	a := 5
+	b := &a
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Printf("%T\n ", a)
+	fmt.Printf("%T\n ", b)
+	fmt.Println("Value of B Address is: ", *b)
+	// Change value with Pointers
+	*b += a
+	fmt.Println("The pre-final Value of B is:  ", *b)
+	a = 20
+	fmt.Println("The final Value of B is:  ", *b)
 }
